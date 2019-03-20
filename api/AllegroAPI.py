813d035cd3aa008513ded2228b913e02258b46da
jagub2 <jagub2@users.noreply.github.com>
2019-03-20 20:17:37 +0100

    more Allegro API fun (not)

--- api/AllegroAPI.py
+++ api/AllegroAPI.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from typing import Pattern
 from urllib.parse import urlparse, ParseResult
+import datetime
 import json
 import re
 import requests
@@ -8,7 +9,7 @@ import time
 
 
 class AllegroAPIHandler:
-    def __init__(self, client_id, client_secret, sandbox=True):
+    def __init__(self, client_id, client_secret, sandbox=True, max_failures=5):
         self.client_id = client_id
         self.client_secret = client_secret
         self.device_code = None
@@ -16,9 +17,10 @@ class AllegroAPIHandler:
         self.api_domain = "allegro.pl"
         self.access_token = None
         self.refresh_token = None
-        self.token_expiry = 0
+        self.token_expiry = None
         self.orig_html_soup = None
         self.orig_url = None
+        self.max_failures = max_failures
         if sandbox:
             self.api_domain = "allegro.pl.allegrosandbox.pl"
 
@@ -34,6 +36,8 @@ class AllegroAPIHandler:
             return response['verification_uri_complete']
 
     def authorize_device(self, failures=0):
+        if failures > self.max_failures:
+            return
         req = requests.post(f"https://{self.api_domain}/auth/oauth/token?grant_type="
                             f"urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code&device_code="
                             f"{self.device_code}",
@@ -42,10 +46,11 @@ class AllegroAPIHandler:
             time.sleep(self.interval)
             self.authorize_device(failures=failures + 1)
         else:
+            current_time = datetime.datetime.now()
             response = json.loads(req.text)
             self.access_token = response['access_token']
             self.refresh_token = response['refresh_token']
-            self.token_expiry = response['expires_in']
+            self.token_expiry = current_time + datetime.timedelta(seconds=int(response['expires_in']))
 
     def call_api(self, url, post=False, data=None):
         if url.startswith('/'):
@@ -61,6 +66,17 @@ class AllegroAPIHandler:
                 "Accept": "application/vnd.allegro.public.v1+json"
             })
         return req
+
+    def renew_tokens(self):
+        req = requests.post(f"https://{self.api_domain}/auth/oauth/token?grant_type=refresh_token"
+                            f"&refresh_token={self.refresh_token}",
+                            auth=(self.client_id, self.client_secret))
+        if req.status_code == requests.codes.ok:
+            current_time = datetime.datetime.now()
+            response = json.loads(req.text)
+            self.access_token = response['access_token']
+            self.refresh_token = response['refresh_token']
+            self.token_expiry = current_time + datetime.timedelta(seconds=int(response['expires_in']))
 
     def call_orig_url(self, url):
         self.orig_url = url
@@ -82,11 +98,13 @@ class AllegroAPIHandler:
                             category = int(category_from_json)
         return category
 
-    def extract_api_filters(self, category_id):
-        category_id_str = ""
+    def extract_api_filters(self, category_id, search_phrase):
+        api_call_str = ""
         if category_id is not None:
-            category_id_str = f"category.id={category_id}"
-        req = self.call_api(f"/offers/listing?{category_id_str}&fallback=true&include=-all&include=filters")
+            api_call_str = f"category.id={category_id}"
+        if search_phrase != '':
+            api_call_str = f"&phrase={search_phrase}"
+        req = self.call_api(f"/offers/listing?{api_call_str}&fallback=true&include=-all&include=filters")
         if req.status_code == requests.codes.ok:
             json_ = json.loads(req.text)
             return json_['filters']
@@ -95,12 +113,16 @@ class AllegroAPIHandler:
     def extract_url_filters(self):
         filters = []
         category_id = self.extract_category_id_from_orig_url()
-        api_filters = self.extract_api_filters(category_id)
         parsed_uri: ParseResult = urlparse(self.orig_url)
+        search_phrase = ''
         for query_param in parsed_uri.query.split('&'):
             param, value = query_param.split('=')
             if param == 'string':
-                filters.append(("phrase", value))
+                filters.append(('phrase', value))
+                search_phrase = value
+        if category_id:
+            filters.append(('category.id', category_id))
+        api_filters = self.extract_api_filters(category_id, search_phrase)
         if self.orig_html_soup:
             filters_pattern: Pattern[str] = re.compile(r"window.__listing_FiltersStoreState\s*=\s*([^;]*);")
             for script in self.orig_html_soup.find_all('script', {'src': False}):
@@ -114,24 +136,38 @@ class AllegroAPIHandler:
                                 for filter_values in filter_['filterValues']:
                                     if filter_values['selected']:
                                         if filter_['id'].isdigit():
-                                            if int(filter_values['raw']['value']) != 1:
-                                                filters.append((f"parameter.{filter_['id']}",
-                                                                f"{filter_values['raw']['value']}"))
-                                            else:
-                                                param_value = re.sub(r'([A-Z])', r'_\1', filter_['raw']['id']). \
-                                                    upper()
-                                                filters.append((f"parameter.{filter_['id']}",
-                                                                f"{param_value}"))
+                                            filters.append(
+                                                normalized_filter_tuple(
+                                                    f"parameter.{filter_['id']}",
+                                                    filter_['raw']['id'],
+                                                    filter_values['raw']['value']
+                                                )
+                                            )
                                         else:
                                             if api_filters:
                                                 for api_filter in api_filters:
                                                     for api_filter_value in api_filter['values']:
                                                         if api_filter_value['name'] == filter_values['name'] or \
                                                                 api_filter_value['name'] == filter_['name']:
-                                                            filters.append((api_filter['id'],
-                                                                            api_filter_value['value']))
+                                                            key_name = api_filter['id']
+                                                            if 'idSuffix' in api_filter_value:
+                                                                key_name += api_filter_value['idSuffix']
+                                                            filters.append(
+                                                                normalized_filter_tuple(
+                                                                    key_name,
+                                                                    filter_values['raw']['id'],
+                                                                    filter_values['raw']['value']
+                                                                )
+                                                            )
 
         return filters
 
     def get_api_domain(self):
         return self.api_domain
+
+
+def normalized_filter_tuple(name, value_to_normalize_if_bool, value):
+    value_in_tuple = value
+    if value.isdigit() and int(value) == 1:
+        value_in_tuple = re.sub(r'([A-Z])', r'_\1', value_to_normalize_if_bool).upper()
+    return name, value_in_tuple
