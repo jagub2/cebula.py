93d39f8bfe24afc7a08b500b8bc2046c94f161e4
jagub2 <jagub2@users.noreply.github.com>
2020-02-04 19:12:44 +0100

    add EbayKleinanzeigen provider

---
+++ providers/EbayKleinanzeigenProvider.py
@@ -0,0 +1,49 @@
+from providers.GenericProvider import *
+from cebula_common import *
+from bs4 import BeautifulSoup
+from collections import deque
+from urllib.parse import urlparse
+import re
+import requests
+
+
+class EbayKleinanzeigenProvider(GenericProvider):
+
+    def __init__(self, queue: deque, config: dict):
+        super(EbayKleinanzeigenProvider, self).__init__(queue, config)
+        self.parsed_uri = urlparse(self.url)
+
+    def get_new_entries(self):
+        req = self.scraper.get(self.url, headers={
+            'User-Agent': self.config['user_agent'],
+            'Accept': '*/*',
+            'Referer': self.url
+        })
+        if req.status_code == requests.codes.ok:
+            soup = BeautifulSoup(req.text, features="lxml")
+            entries = {}
+            entries_ids = []
+            for offer in soup.find_all('article', {'class': 'aditem'}):
+                id_ = sha1sum(f"{self.config['url']}{offer['data-adid']}")
+                link = offer.find('a', {'class': 'ellipsis'})
+                url = link['href'].strip()
+                if url.startswith('/'):
+                    url = f"{self.parsed_uri.scheme}://{self.parsed_uri.netloc}{url}"
+                title = link.text.strip()
+                photo_url = None
+                if 'include_photos' in self.config and self.config['include_photos']:
+                    image = offer.find('div', {'class': 'imagebox'})['data-imgsrc']
+                    if image:
+                        photo_url = image['src']
+                entries[id_] = {
+                    'link': url,
+                    'title': title
+                }
+                if photo_url:
+                    entries[id_]['photo'] = photo_url
+                entries_ids.append(id_)
+
+            new_entries_id = [entry for entry in entries_ids if entry not in self.ids]
+            return new_entries_id, entries
+        else:
+            pass
--- providers/__init__.py
+++ providers/__init__.py
@@ -4,3 +4,4 @@ from providers import OLXProvider
 from providers import PepperProvider
 from providers import RSSProvider
 from providers import WordpressProvider
+from providers import EbayKleinanzeigenProvider
