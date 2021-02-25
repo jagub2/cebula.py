1502d5266b3a0f8c88073a87c7eebac290fd2676
jagub2 <jagub2@users.noreply.github.com>
2021-02-25 07:11:17 +0100

    better exception handling

--- bots/MessengerBot.py
+++ bots/MessengerBot.py
@@ -1,8 +1,12 @@
+from loguru import logger
+from cebula_common import for_all_methods
 from messengers import *
+import time
 import threading
 import messengers
 
 
+@for_all_methods(logger.catch)
 class MessengerThread(threading.Thread):
 
     def __init__(self, bot_class_str: str, *args, **kwargs):
@@ -15,8 +19,14 @@ class MessengerThread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        self.bot.connect()
-        self.bot.queue_loop()
+        try:
+            self.bot.connect()
+            self.bot.queue_loop()
+        except Exception as e:
+            logger.error(f"MessengerThread: Got exception: {e}")
+            traceback.print_stack()
+            time.sleep(10)
+            self.run()
 
     def halt(self):
         self.bot.halt()
