cad7d0f6d991ee3295f98bc7b3946d14e841ff07
jagub2 <jagub2@users.noreply.github.com>
2021-03-03 22:30:17 +0100

    introduce new allegro provider due to planned ridiculous API restrictions

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,5 +1,6 @@
 from providers import GenericProvider
 from providers import AllegroProvider
+from providers import AllegroScraperProvider
 from providers import OLXProvider
 from providers import PepperProvider
 from providers import RSSProvider
