536aa816ffe4c428aa25951dfaa5643cfab19608
jagub2 <jagub2@users.noreply.github.com>
2021-10-28 23:05:30 +0200

    make ExternalProvide working

--- providers/ExternalProvider.py
+++ providers/ExternalProvider.py
@@ -7,23 +7,20 @@ import json
 @for_all_methods(logger.catch)
 class ExternalProvider(GenericProvider):
 
-    def __init__(self, queue: deque, config: dict, id_list: IdList):
-        super().__init__(queue, config, id_list)
-        self.redis_key = self.config['redis_key']
-
     def get_new_entries(self) -> dict:
         entries = {}
-        external_entries = self.id_list.redis.lrange(self.redis_key)
+        external_entries = self.id_list.redis.lrange(self.config['redis_key'], 0, -1)
         if len(external_entries) > 0:
             for json_entry in external_entries:
                 try:
                     entry = json.loads(json_entry)
-                    id = hashlib.md5(f"{entry[0]}-{entry[1]}")
-                    entries[id] = {
+                    id_ = hashlib.md5(f"{entry[0]}-{entry[1]}".encode('utf-8')).hexdigest()
+                    entries[id_] = {
                         'link': entry[1],
                         'title': entry[0]
                     }
                 except json.decoder.JSONDecodeError:
                     continue
+        self.id_list.redis.ltrim(self.config['redis_key'], len(external_entries), -1)
 
         return entries
